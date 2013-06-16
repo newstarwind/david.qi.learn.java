7944e47d59852961db1353b11168ee2bacfad06d
newstarwind <iam.qixin@gmail.com>
2013-06-16 16:43:07 +0800

    no changes

--- david.qi.learn.java/src/david/qi/learn/java/concurrency/threads/MyRunnable.java
+++ david.qi.learn.java/src/david/qi/learn/java/concurrency/threads/MyRunnable.java
@@ -12,8 +12,7 @@ public class MyRunnable implements Runnable {
      MyRunnable(long countUntil) { 
           this.countUntil = countUntil; 
      } 
-     @Override 
-     public void run() { 
+     @Override public void run() { 
           long sum = 0; 
           for (long i = 1; i < countUntil; i++) { sum += i; } 
           System.out.println(sum); 
