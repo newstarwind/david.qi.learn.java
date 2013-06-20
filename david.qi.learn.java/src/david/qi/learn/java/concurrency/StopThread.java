c06ac1284ff08046c1e3b29468c5ec10e3ad869f
newstarwind <iam.qixin@gmail.com>
2013-06-20 18:12:22 +0800

    add thread relevant class

---
+++ david.qi.learn.java/src/david/qi/learn/java/concurrency/StopThread.java
@@ -0,0 +1,32 @@
+package david.qi.learn.java.concurrency;
+
+import java.util.concurrent.TimeUnit;
+
+public class StopThread {
+	private static boolean stopRequested;
+	
+	
+	private static synchronized void requestStop() {
+		stopRequested = true;
+	}
+	
+	//注意写方法和读方法都被同步, 只同步写方法还不够.
+	private static synchronized boolean stopRequested() {
+		return stopRequested;
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		Thread backgroundThread = new Thread(new Runnable() {
+			@Override public void run() {
+				int i = 0;
+				while(!stopRequested())
+					i++;
+				System.out.println(i);
+			}
+		});
+		backgroundThread.start();
+		
+		TimeUnit.SECONDS.sleep(2);
+		requestStop();
+	}
+}
---
+++ david.qi.learn.java/src/david/qi/learn/java/concurrency/Synchronizer.java
@@ -0,0 +1,52 @@
+package david.qi.learn.java.concurrency;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Synchronizer {
+
+	public static long time(Executor executor, int concurrency, final Runnable action) throws InterruptedException{
+		final CountDownLatch ready = new CountDownLatch(concurrency);
+		final CountDownLatch start	  = new CountDownLatch(1);
+		final CountDownLatch done  = new CountDownLatch(concurrency);
+		for(int i = 0; i < concurrency; i++) {
+			executor.execute(new Runnable() {
+				public void run() {
+					ready.countDown();  //Tell timer we're ready
+					try {
+						start.await();	//Wait till peers are ready
+						action.run();
+					}catch(InterruptedException e) {
+						Thread.currentThread().interrupt();
+					}finally {
+						done.countDown();  //Tell timer we're done
+					}
+				}	
+			});			
+		}
+		
+		ready.await();				//Wait for all workers to be ready
+		long startNanos = System.nanoTime();
+		start.countDown(); 	// And they're off!
+		done.await();				// Wait for all workers to finish.
+		return System.nanoTime() - startNanos;
+		
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+	    Executor executor = Executors.newCachedThreadPool();
+	    int concurrency = 4;
+	    long time = time(executor, concurrency, new Runnable() {
+	    	public void run() {
+	    		StringBuilder s = new StringBuilder("-");
+	    		while(s.length() < 10) {
+	    			System.out.print(s);
+	    			s.append("-");
+	    		}
+	    		System.out.println(">");
+	    	}
+	    });
+	    System.out.println(time);
+    }
+}
